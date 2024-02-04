33f66131667edde6fab62ae335ba867fc04fd1f7
André Baceti <a.baceti@murabei.com>
2024-02-04 16:51:20 -0300

    Building a new version 1.31

--- src/pumpwood_djangoauth/i8n/translate.py
+++ src/pumpwood_djangoauth/i8n/translate.py
@@ -4,10 +4,13 @@ from lazy_string import LazyString
 
 def aux_translate_string(sentence, tag, plural, language, user_type):
     """Translate string using microservice."""
-    from pumpwood_djangoauth.config import pumpwood_i8n
-    return pumpwood_i8n.t(
-        sentence=sentence, tag=tag, plural=plural,
-        language=language, user_type=user_type)
+    try:
+        from pumpwood_djangoauth.config import PumpwoodI8nTranslation
+        return PumpwoodI8nTranslation.translate(
+            sentence=sentence, tag=tag, plural=plural,
+            language=language, user_type=user_type)
+    except Exception:
+        return sentence
 
 
 def t(sentence, tag='', plural=False, language='', user_type=''):
